b7290a0fdb41ebf770d02818364d878c7d030478
Thatblue8500 <leonardburris@icloud.com>
2026-04-23 15:30:54 +0000

    finished curcularity

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,19 +20,29 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle
+        var circles = []
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+physikz.addRandomVelocity(circle, canvas, 5, 5);
+view.addChild(circle);
+circles.push(circle);
+        }
 
 
         // TODO 3 : Call the drawCircle() function
-
-
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
+        drawCircle()
 
         // TODO 7 : Use a loop to create multiple circles
-
+       for(var i = 1; 1 < 500 1 += 2
+        drawCircle
 
 
 
@@ -47,8 +57,8 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-
-            
+            circles[0]
+            physikz.updatePosition
             // TODO 5 : Call game.checkCirclePosition() on your circles
            
 
@@ -96,5 +106,3 @@ var init = function (window) {
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
-    module.exports = init;
-}
